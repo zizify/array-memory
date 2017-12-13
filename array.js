5a4d00f323a4e8256689e253c81cbeb1695206ce
Andy Gaines <andyagaines@gmail.com>
2017-12-13 11:10:26 -0600

    finished initial version of array.js

--- array.js
+++ array.js
@@ -42,12 +42,63 @@ class Array {
 		this._capacity = size;
 	}
     
+	//create method to get value stored in array
 	get(index) {
+		//checks that array index is valid
 		if (index < 0 || index >= this.length) {
 			throw new Error('index error');
 		}
 
+		//use function get to retrieve value at the index location, which it this.ptr + index
 		return memory.get(this.ptr + index);
+	}
+    
+	//create method to pop last value off of the array
+	pop() {
+		//checks that array length is valid
+		if (this.length === 0) {
+			throw new Error('index error');
+		}
+
+		//gets value from memory with get function using the ptr and the length of the array
+		//decrements array length
+		const value = memory.get(this.ptr + this.length - 1);
+		this.length--;
+        
+		return value;
+	}
+    
+	//creates method to insert a value at a particular index
+	insert(index, value) {
+		//checks that index is valid
+		if (index < 0 || index >= this.length) {
+			throw new Error('index error');
+		}
+
+		//allocates more space if the length is equal or greater than the capacity
+		if (this.length >= this._capacity) {
+			this._resize((this.length + 1) * Array.SIZE_RATIO);
+		}
+        
+		//copies data and moves it forward one position
+		//sets new value at the index
+		//increments length
+		memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+		memory.set(this.ptr + index, value);
+		this.length++;
+	}
+    
+	//creates method to remove an item at a particular index
+	remove(index) {
+		//checks that index is valid
+		if (index < 0 || index >= this.length) {
+			throw new Error('index error');
+		}
+
+		//copies data backwards one position to overwrite data starting at the index given
+		//decrements length
+		memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+		this.length--;
 	}
 }
 
